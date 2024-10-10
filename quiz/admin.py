3b1defd1f70ce3c9a8a1f7f50004b61807f561c9
ahmadalsajid <ahmadalsajid@gmail.com>
2024-10-11 00:54:08 +0600

    tag parent added for questions

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,6 @@
+from pprint import pprint
 from django.contrib import admin
-from quiz.models import Tag
+from quiz.models import Tag, Question
 
 
 class TagInline(admin.TabularInline):
@@ -12,4 +13,26 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'right_answer',)
+    search_fields = ('question',)
+
+    def save_related(self, request, form, formsets, change):
+        pprint(form.cleaned_data)
+        _selected_tags = form.cleaned_data['tags']
+        _tags = []
+        if _selected_tags:
+            for t in _selected_tags:
+                while t:
+                    _tags.append(t)
+                    if not t.parent:
+                        break
+                    t = t.parent
+        _tags = list(set(_tags))
+        print(_tags)
+        form.cleaned_data['tags'] = _tags
+        super(QuestionAdmin, self).save_model(request, form, formsets, change)
+
+
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Question, QuestionAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+from users.models import User
+from icecream import ic
+from pprint import pprint
+from django.db import connection, reset_queries
 
 
-class Tags(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True, db_index=True)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='tags', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,3 +18,37 @@ class Tags(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class Question(models.Model):
+    question = models.TextField(db_index=True)
+    option_1 = models.CharField(max_length=255, null=True, blank=True)
+    option_2 = models.CharField(max_length=255, null=True, blank=True)
+    option_3 = models.CharField(max_length=255, null=True, blank=True)
+    option_4 = models.CharField(max_length=255, null=True, blank=True)
+    right_answer = models.IntegerField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='questions', blank=True)
+    users = models.ManyToManyField(User, related_name='questions', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        ordering = ['-created_at']
+
+# @receiver(pre_save, sender=Question)
+# def set_question_Tags(sender, instance, **kwargs):
+#     try:
+#         _tags = []
+#         reset_queries()
+#         for t in instance.tags.select_related('parent').all():
+#             while t:
+#                 _tags.append(t)
+#                 if not t.parent:
+#                     break
+#                 t = t.parent
+#         instance.tags.set(list(set(_tags)))
+#     except Exception as e:
+#         pass
